be676c2357cb809c1149a6a9ddeaaaee4ab7da1e
devkkiri <yoon_1139@naver.com>
2023-05-24 16:31:07 +0900

    Feat: Section 1

--- src/components/Section1/Section1.styles.ts
+++ src/components/Section1/Section1.styles.ts
@@ -3,89 +3,83 @@ import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 
 export const Section1Container = styled.div<IsDesktop>`
-  background: url('/assets/images/section1/background_mobile.png') center;
-  background-size: cover;
-  margin-top: -62px;
+  position: relative;
+  padding-top: 85px;
+  padding-bottom: 90px;
 
   ${({isDesktop}) =>
     isDesktop &&
     css`
-      background: url('/assets/images/section1/background_desktop.png') center;
-      background-size: cover;
-      margin-top: -82px;
+      padding-top: 88px;
+      padding-bottom: 94px;
     `};
+`;
+
+export const Section1Background = styled.div<IsDesktop>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 360px;
+  z-index: -1;
+
+  background: url('/assets/images/section1/section1_mobile.png') center;
+  background-size: cover;
+
+  ${({isDesktop}) =>
+    isDesktop &&
+    css`
+      background: url('/assets/images/section1/section1_desktop.png') center;
+      background-size: cover;
+
+      top: 56px;
+      width: 951px;
+      left: calc((${window.innerWidth}px - 951px) / 2);
+      height: 400px;
+    `}
 `;
 
 export const Section1Block = styled.div<IsDesktop>`
-  width: fit-content;
-  padding-top: 62px;
-  padding-bottom: 29.38px;
+  width: 343px;
   margin: 0 auto;
-  padding: 0 16px;
 
   display: flex;
   flex-direction: column;
-  gap: 58.55px;
 
   ${({isDesktop}) =>
     isDesktop &&
     css`
-      padding-top: 142px;
-      padding-bottom: 70px;
-      flex-direction: row;
-      gap: 56px;
-      justify-content: space-between;
+      width: 1056px;
+      align-items: center;
     `};
 `;
 
-export const Section1TitleBox = styled.div<IsDesktop>`
+export const Section1TitleBlock = styled.div<IsDesktop>`
+  font-weight: 800;
+  font-size: 66px;
+  line-height: 66px;
+  letter-spacing: -0.717504px;
+  white-space: pre-wrap;
+
   display: flex;
   flex-direction: column;
-  padding-top: 72px;
+  justify-content: flex-end;
 
-  font-weight: 400;
-  font-size: 42px;
-  line-height: 52px;
-  letter-spacing: -0.6px;
+  margin-bottom: 63px;
 
-  margin-bottom: 24px;
-
-  b {
-    font-weight: 800;
-    white-space: pre-wrap;
+  span {
+    text-align: right;
   }
 
   ${({isDesktop}) =>
     isDesktop &&
     css`
-      padding-top: 60px;
-      font-size: 70px;
-      line-height: 88px;
-      letter-spacing: -0.9px;
-      margin-bottom: 12px;
+      width: 460px;
+
+      font-size: 111px;
+      line-height: 111px;
+      letter-spacing: -0.953906px;
     `};
-`;
-
-export const Section1SubTitle = styled.div<IsDesktop>`
-  font-weight: 700;
-  font-size: 21px;
-  line-height: 28px;
-  letter-spacing: -0.4px;
-
-  margin-bottom: 24px;
-
-  b {
-    color: ${colors.MAIN_COLOR};
-  }
-
-  ${({isDesktop}) =>
-    isDesktop &&
-    css`
-      font-size: 36px;
-      line-height: 48px;
-      letter-spacing: -0.6px;
-      margin-bottom: 48px;
-    `}
 `;
 
 export const Section1TextBox = styled.div<IsDesktop>`
@@ -94,7 +88,17 @@ export const Section1TextBox = styled.div<IsDesktop>`
   line-height: 28px;
   letter-spacing: -0.3px;
   white-space: pre-wrap;
-  margin-bottom: 24px;
+
+  margin-bottom: 32px;
+
+  strong {
+    display: block;
+    font-weight: 700;
+    font-size: 21px;
+    line-height: 28px;
+    letter-spacing: -0.4px;
+    margin-bottom: 16px;
+  }
 
   b {
     font-weight: 700;
@@ -104,6 +108,7 @@ export const Section1TextBox = styled.div<IsDesktop>`
   ${({isDesktop}) =>
     isDesktop &&
     css`
+      text-align: center;
       font-size: 21px;
       line-height: 32px;
       letter-spacing: -0.4px;
@@ -113,6 +118,7 @@ export const Section1TextBox = styled.div<IsDesktop>`
 export const WellCommBlock = styled.div<IsDesktop>`
   background: rgba(225, 225, 234, 0.5);
   border-radius: 12px;
+  white-space: pre-wrap;
 
   display: flex;
   padding: 16px;
@@ -127,15 +133,10 @@ export const WellCommBlock = styled.div<IsDesktop>`
     font-weight: 800;
   }
 
-  @media (min-width: 405px) {
-    white-space: pre-wrap;
-  }
-
   ${({isDesktop}) =>
     isDesktop &&
     css`
       gap: 12px;
-      white-space: unset;
       max-width: 515px;
 
       font-size: 18px;
--- src/components/Section1/Section1.tsx
+++ src/components/Section1/Section1.tsx
@@ -2,10 +2,11 @@
 import Image from 'next/image';
 import React, {useEffect} from 'react';
 import {
+  Section1Background,
   Section1Block,
   Section1Container,
   Section1TextBox,
-  Section1TitleBox,
+  Section1TitleBlock,
   WellCommBlock,
 } from './Section1.styles';
 
@@ -16,45 +17,54 @@ const Section1 = ({isDesktop}: IsDesktop) => {
 
   return (
     <Section1Container isDesktop={isDesktop}>
+      <Section1Background isDesktop={isDesktop} />
+
       <Section1Block isDesktop={isDesktop}>
-        <div>
-          <Section1TitleBox isDesktop={isDesktop}>
-            <b>{'Well Comm\nTalk'}</b>
-          </Section1TitleBox>
+        <Section1TitleBlock isDesktop={isDesktop}>
+          Well{'\n'}Comm.
+          <span>Talk</span>
+        </Section1TitleBlock>
 
-          <Section1TextBox isDesktop={isDesktop}>
-            <p>
-              {
-                "2023년 회사는, 그리고 구성원 개개인은\n어떤 목표를 향해 달려가야 할까요?\n우리 회사의 경영현안과 방향성 등에 대해\n공유하는 채널인 '웰컴 톡'을 시작합니다!\n회사에 대한 이해도를 높이고 모두가 함께 이야기 하는\n웰푸드만의 소통 채널이 될 수 있도록\n임직원 여러분의 많은 관심 부탁드립니다."
-              }
-            </p>
-          </Section1TextBox>
+        <Section1TextBox isDesktop={isDesktop}>
+          <strong>
+            {
+              '2023년 회사는, 그리고 구성원 개개인은\n어떤 목표를 향해 달려가야 할까요?'
+            }
+          </strong>
+          <p>
+            우리 회사의 경영현안과 방향성 등에 대해{'\n'}공유하는 채널인{' '}
+            <b>'웰컴 톡'</b>을 시작합니다!{'\n'}회사에 대한 이해도를 높이고
+            모두가 함께 이야기 하는{'\n'}웰푸드만의 소통 채널이 될 수 있도록
+            {'\n'}임직원 여러분의 많은 관심 부탁드립니다.
+          </p>
+        </Section1TextBox>
 
-          <WellCommBlock isDesktop={isDesktop}>
-            <Image
-              src="/assets/images/section1/chats-heart.png"
-              width={isDesktop ? 64 : 48}
-              height={isDesktop ? 64 : 48}
-              alt="wellfood"
-              priority
-            />
+        <WellCommBlock isDesktop={isDesktop}>
+          <Image
+            src="/assets/images/section1/chats-heart.png"
+            width={isDesktop ? 64 : 48}
+            height={isDesktop ? 64 : 48}
+            alt="wellfood"
+            priority
+          />
 
-            <p>
-              '웰컴 톡'은 웰푸드의 <b>Well</b>과 <b>Comm</b>unication 의{'\n'}
-              전파/공유의 뜻을 담아 다양한 이야기를 함께 나누는{'\n'}
-              소통 채널의 의미를 담고 있습니다 {':)'}
-            </p>
-          </WellCommBlock>
-        </div>
-
-        <Image
-          src="/assets/images/section1/graphic.png"
-          width={isDesktop ? 520 : 343}
-          height={isDesktop ? 687 : 453}
-          alt="wellfood"
-          priority
-          style={isDesktop ? {} : {margin: '0 auto'}}
-        />
+          <p>
+            {isDesktop ? (
+              <>
+                '웰컴 톡'은 웰푸드의 <b>Well</b>과 <b>Comm</b>unication 의{'\n'}
+                전파/공유의 뜻을 담아 다양한 이야기를 함께 나누는{'\n'}
+                소통 채널의 의미를 담고 있습니다 {':)'}
+              </>
+            ) : (
+              <>
+                '웰컴 톡'은 웰푸드의 <b>Well</b>과{'\n'}
+                <b>Comm</b>unication 의{'\n'}
+                전파/공유의 뜻을 담아{'\n'}다양한 이야기를 함께 나누는{'\n'}
+                소통 채널의 의미를 담고 있습니다 {':)'}
+              </>
+            )}
+          </p>
+        </WellCommBlock>
       </Section1Block>
     </Section1Container>
   );
